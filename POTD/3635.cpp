0c509a881b258499aeeaadfb5df55b28556876e9
Anchal Gupta <csrl.anchalgupta@gmail.com>
2026-06-04 22:11:38 +0530

    Today's POTD Solved 03-06-2026

---
+++ POTD/3635.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>& start1, vector<int>& duration1, vector<int>& start2,
+              vector<int>& duration2) {
+        int finish1 = INT_MAX;
+        for (int i = 0; i < start1.size(); i++) {
+            finish1 = min(finish1, start1[i] + duration1[i]);
+        }
+
+        int finish2 = INT_MAX;
+        for (int i = 0; i < start2.size(); i++) {
+            finish2 = min(finish2, max(start2[i], finish1) + duration2[i]);
+        }
+        return finish2;
+    }
+
+    int earliestFinishTime(vector<int>& landStartTime,
+                           vector<int>& landDuration,
+                           vector<int>& waterStartTime,
+                           vector<int>& waterDuration) {
+        int land_water =
+            solve(landStartTime, landDuration, waterStartTime, waterDuration);
+        int water_land =
+            solve(waterStartTime, waterDuration, landStartTime, landDuration);
+        return min(land_water, water_land);
+    }
+
+    int main(){
+        int n,m;
+        cin>>n>>m;
+        vector<int>landStartTime(n),landDuration(n),waterStartTime(m),waterDuration(m);
+        for(int i=0;i<n;i++) cin>>landStartTime[i];
+        for(int i=0;i<n;i++) cin>>landDuration[i];
+        for(int i=0;i<m;i++) cin>>waterStartTime[i];
+        for(int i=0;i<m;i++) cin>>waterDuration[i];
+        cout<<earliestFinishTime(landStartTime, landDuration, waterStartTime, waterDuration)<<endl;
+    }
+    
