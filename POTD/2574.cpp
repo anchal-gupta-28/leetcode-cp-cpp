b6c66a145539810fd2925cf6d074f544cde1d24e
Anchal Gupta <csrl.anchalgupta@gmail.com>
2026-06-07 00:28:37 +0530

    Optimized solution for todays potd

--- POTD/2574.cpp
+++ POTD/2574.cpp
@@ -2,32 +2,56 @@
     #include<vector>
     using namespace std;
 
+    // Optimized one...
     vector<int> leftRightDifference(vector<int>& nums) {
         int n=nums.size();
-
-        vector<int>leftSum(n);
-        int curr=0;
-        leftSum[0]=0;
-        for(int i=1;i<n;i++){
-            curr+=nums[i-1];
-            leftSum[i] = curr;
-        }
-
-        vector<int>rightSum(n);
-        curr+=nums[n-1];
-        for(int i=0;i<n-1;i++){
-            curr = curr-nums[i];
-            rightSum[i] = curr;
-        }
-        rightSum[n-1] = 0;
+        int total=0;
+        for(int ele:nums) total+=ele;
 
         vector<int>ans(n);
-        for(int i=0;i<n;i++){
-            ans[i] = abs(leftSum[i]-rightSum[i]);
+        int leftSum=0;
+        int rightSum=total-nums[0];
+
+        ans[0] = rightSum;
+        for(int i=1;i<n;i++){
+            leftSum +=nums[i-1];
+            rightSum-=nums[i];
+            int curr = abs(leftSum-rightSum);
+            ans[i]=curr;
         }
 
         return ans;
     }
+
+    // Brute force....
+    // vector<int> leftRightDifference(vector<int>& nums) {
+    //     int n=nums.size();
+
+    //     vector<int>leftSum(n);
+    //     int curr=0;
+    //     leftSum[0]=0;
+    //     for(int i=1;i<n;i++){
+    //         curr+=nums[i-1];
+    //         leftSum[i] = curr;
+    //     }
+
+    //     vector<int>rightSum(n);
+    //     curr+=nums[n-1];
+    //     for(int i=0;i<n-1;i++){
+    //         curr = curr-nums[i];
+    //         rightSum[i] = curr;
+    //     }
+    //     rightSum[n-1] = 0;
+
+    //     vector<int>ans(n);
+    //     for(int i=0;i<n;i++){
+    //         ans[i] = abs(leftSum[i]-rightSum[i]);
+    //     }
+
+    //     return ans;
+    // }
+
+
 
     int main(){
         vector<int> nums={10,4,8,3};
